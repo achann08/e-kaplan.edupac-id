ceb241e491cca45b039a2c80745c805e048fc812
achann08 <hendratrisno08@gmail.com>
2025-01-18 20:58:01 +0700

    change configuration moodle to filess.io

--- config.php
+++ config.php
@@ -6,20 +6,22 @@ $CFG = new stdClass();
 
 $CFG->dbtype    = 'mariadb';
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = 'localhost';
-$CFG->dbname    = 'edupacid_mood582';
-$CFG->dbuser    = 'root';
-$CFG->dbpass    = '';
+$CFG->dbhost    = 'jfg03.h.filess.io';
+$CFG->dbname    = 'edupacidmood582_winherself';
+$CFG->dbuser    = 'edupacidmood582_winherself';
+$CFG->dbpass    = '557d7e590fca95ab02522c20ff4a44bd1f3429d4';
 $CFG->prefix    = 'mdllu_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
-  'dbport' => '',
+  'dbport' => 3305,
   'dbsocket' => '',
   'dbcollation' => 'utf8mb4_general_ci',
 );
 
+
+
 $CFG->wwwroot   = 'http://localhost/e-kaplan.edupac-id';
-$CFG->dataroot  = 'C:\\xampp\\moodledata';
+$CFG->dataroot  = 'E:\\XAMPP\\moodledata';
 $CFG->admin     = 'admin';
 
 $CFG->directorypermissions = 0777;
